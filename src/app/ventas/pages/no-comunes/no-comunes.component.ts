e7e7890b812536ac2e21fa5ed9ddf097cf4ba78f
sebasGZA <sebassga34@gmail.com>
2022-01-10 16:27:18 -0500

    il18nPluralPipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -8,11 +8,20 @@ import { Component } from '@angular/core';
 })
 export class NoComunesComponent {
 
+  //i18nSelect
   nombre: string = 'Susana'
   genero: string = 'femenino'
   invitacionMap = {
-    'masculino':'invitarlo',
-    'femenino':'invitarla'
+    'masculino': 'invitarlo',
+    'femenino': 'invitarla'
   }
 
+  //i18nPlural
+
+  clientes: string[] = ['Maria', 'Pedro', 'Juan']
+  clientesMap = {
+    '=0': 'no tenemos ningun cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    'other': 'tenemos # clientes esperando.'
+  }
 }
