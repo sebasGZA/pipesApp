c3d36ff9ab32d776f0acf84f32f31cf6aa6b8583
sebasGZA <sebassga34@gmail.com>
2022-01-10 13:41:42 -0500

    primeNg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
 
-//PrimeNG
-import { ButtonModule } from 'primeng/button';
-import {CardModule} from 'primeng/card';
 
 @NgModule({
   declarations: [
@@ -13,8 +11,7 @@ import {CardModule} from 'primeng/card';
   ],
   imports: [
     BrowserModule,
-    ButtonModule,
-    CardModule
+    PrimeNgModule
   ],
   providers: [],
   bootstrap: [AppComponent]
