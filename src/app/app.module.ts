b46c7b8207df7d14ee2f3c2c26ab3e77d7c99c05
sebasGZA <sebassga34@gmail.com>
2022-01-10 14:08:34 -0500

    rutas de aplicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRouterModule } from './app-router.module';
 import { SharedModule } from './shared/shared.module';
 
 
@@ -11,7 +12,8 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    SharedModule
+    AppRouterModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
