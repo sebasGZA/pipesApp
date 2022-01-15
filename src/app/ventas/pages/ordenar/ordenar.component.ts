517519d2e62b18280327a39655d0f62c60524fa0
sebasGZA <sebassga34@gmail.com>
2022-01-15 09:30:13 -0500

    Llenar un PrimeTable con data

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interface';
 
 @Component({
   selector: 'app-ordenar',
@@ -9,6 +10,33 @@ import { Component } from '@angular/core';
 export class OrdenarComponent {
 
   enMayuscula: boolean = true
+  heroes: Heroe[] = [
+    {
+      nombre: 'superman',
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde
+    },
+    {
+      nombre: 'Daredevil',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Linterna verde',
+      vuela: true,
+      color: Color.verde
+    }
+  ]
 
   cambiarMayuscula() {
     this.enMayuscula = !this.enMayuscula
